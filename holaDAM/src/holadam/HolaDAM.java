a9c423f31c51aaf132e5521649f10e28e4c065e0
Alex_Informatic <alexanderadell2003@gmail.com>
2023-01-21 17:48:16 +0100

    Segunda modificación

--- holaDAM/src/holadam/HolaDAM.java
+++ holaDAM/src/holadam/HolaDAM.java
@@ -6,6 +6,9 @@ public class HolaDAM {
 
     public static void main(String[] args) {
         System.out.println("Hola DAM");
+        int a=5;
+        int b=10;
+        int c=a+b;
     }
     
 }

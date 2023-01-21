6f7a3424b48864abe3dd1b06dafbc0dbe26318af
Alex <118816743+alexhub016@users.noreply.github.com>
2023-01-21 23:48:56 +0100

    Update HolaDAM.java

--- holaDAM/src/holadam/HolaDAM.java
+++ holaDAM/src/holadam/HolaDAM.java
@@ -5,7 +5,7 @@ package holadam;
 public class HolaDAM {
 
     public static void main(String[] args) {
-        System.out.println("Hola DAM");
+        System.out.println("Hola DAM modificado");
         int a=5;
         int b=10;
         int c=a+b;
